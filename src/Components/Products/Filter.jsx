45ff3e80b0c79b4569354c3c9a81af622e150931
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-05-09 19:38:54 +0530

    product Task added

---
+++ src/Components/Products/Filter.jsx
@@ -0,0 +1,13 @@
+import "./Filter.css";
+export const Filter = () => {
+  return (
+    <div>
+      <select name="" id="" className="width">
+        <option value="">Filter</option>
+        <option value="price">price</option>
+        <option value="Asc">Ascending</option>
+        <option value="Desc">descending</option>
+      </select>
+    </div>
+  );
+};
